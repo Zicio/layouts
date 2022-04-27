8b76fc5cd8fc03aa35e4465616fe85bf0bc33502
Zicio <zicio@yandex.ru>
2022-04-27 16:52:20 +0400

    Updated styles

--- src/Types/ShopCard.js
+++ src/Types/ShopCard.js
@@ -5,7 +5,7 @@ function ShopCard(props) {
       <p className="card__name">{card.name}</p>
       <span className="card__color">{card.color}</span>
       <img className="card__image" src={card.img} alt="Изображение" />
-      <span className="card__price">{card.price}</span>
+      <span className="card__price">${card.price}</span>
       <button className="card__add" type="button">
         add to cart
       </button>
--- src/Types/ShopItem.js
+++ src/Types/ShopItem.js
@@ -5,7 +5,7 @@ function ShopItem(props) {
       <img className="item__image" src={item.img} alt="Изображение" />
       <p className="item__name">{item.name}</p>
       <span className="item__color">{item.color}</span>
-      <span className="item__price">{item.price}</span>
+      <span className="item__price">${item.price}</span>
       <button className="item__add" type="button">
         add to cart
       </button>
